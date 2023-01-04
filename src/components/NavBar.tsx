1f9df56a25b593812ffa0ac0431c4e19642cc944
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-01-04 19:05:58 +0100

    Basic header

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return <div className="flex justify-center items-center h-16 border-b-2">
+    <div className="text-2xl font-bold">Wordle Clone</div>
+  </div >
+}
+
+export default NavBar
