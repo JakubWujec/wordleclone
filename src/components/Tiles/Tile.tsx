037226c9eaadfbc93b71d9d58bc94af8a7e90259
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-01-23 21:52:44 +0100

    non selectable cells

--- src/components/Tiles/Tile.tsx
+++ src/components/Tiles/Tile.tsx
@@ -9,14 +9,14 @@ const Cell = (props: CellProps) => {
   if (props.state === "UNCHECKED") {
     if (props.char === EMPTY_CHAR) {
       return (
-        <div className="w-12 h-12 border-2 flex justify-center items-center font-bold text-xl">
+        <div className="select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl">
           { }
         </div>
       )
     }
 
     return (
-      <div className="w-12 h-12 border-2 border-slate-800 flex justify-center items-center font-bold text-xl">
+      <div className="select-none w-12 h-12 border-2 border-slate-800 flex justify-center items-center font-bold text-xl">
         {props.char}
       </div>
     )
@@ -24,7 +24,7 @@ const Cell = (props: CellProps) => {
 
   if (props.state === "WRONG") {
     return (
-      <div className="w-12 h-12  flex justify-center items-center font-bold text-xl bg-slate-500 text-white">
+      <div className="select-none w-12 h-12  flex justify-center items-center font-bold text-xl bg-slate-500 text-white">
         {props.char}
       </div>
     )
@@ -32,7 +32,7 @@ const Cell = (props: CellProps) => {
 
   if (props.state === "MISPLACED") {
     return (
-      <div className="w-12 h-12 flex justify-center items-center font-bold text-xl bg-yellow-500 text-white">
+      <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-xl bg-yellow-500 text-white">
         {props.char}
       </div>
     )
@@ -40,14 +40,14 @@ const Cell = (props: CellProps) => {
 
   if (props.state === "CORRECT") {
     return (
-      <div className="w-12 h-12 flex justify-center items-center font-bold text-xl bg-green-500 text-white">
+      <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-xl bg-green-500 text-white">
         {props.char}
       </div>
     )
   }
 
   return (
-    <div className="w-12 h-12 flex justify-center items-center font-bold text-x">
+    <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-x">
       ERR
     </div>
   )
