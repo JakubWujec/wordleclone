4c9b0ca65376b1564c2b46320ed7ca6ab3530696
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-03-30 21:21:31 +0200

    shortened cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -5,54 +5,28 @@ type CellProps = {
   wordleCell: WordleCell
 }
 
+let extraClassName = {
+  CORRECT: 'bg-green-500 text-white',
+  MISPLACED: 'bg-yellow-500 text-white',
+  WRONG: 'bg-slate-500 text-white',
+  UNCHECKED: 'border-slate-800'
+}
+
 
 const Cell = ({ wordleCell }: CellProps) => {
-  if (wordleCell.status === "UNCHECKED") {
-    if (wordleCell.char === WORDLE_EMPTY_CHAR) {
-      return (
-        <div className="select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl">
-          { }
-        </div>
-      )
-    }
+  const isEmpty = wordleCell.char === WORDLE_EMPTY_CHAR
 
-    return (
-      <div className="select-none w-12 h-12 border-2 border-slate-800 flex justify-center items-center font-bold text-xl">
-        {wordleCell.char}
-      </div>
-    )
-  }
-
-  if (wordleCell.status === "WRONG") {
-    return (
-      <div className="select-none w-12 h-12  flex justify-center items-center font-bold text-xl bg-slate-500 text-white">
-        {wordleCell.char}
-      </div>
-    )
-  }
-
-  if (wordleCell.status === "MISPLACED") {
-    return (
-      <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-xl bg-yellow-500 text-white">
-        {wordleCell.char}
-      </div>
-    )
-  }
-
-  if (wordleCell.status === "CORRECT") {
-    return (
-      <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-xl bg-green-500 text-white">
-        {wordleCell.char}
-      </div>
-    )
+  if (isEmpty) {
+    return <div className="select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl">
+      { }
+    </div>
   }
 
   return (
-    <div className="select-none w-12 h-12 flex justify-center items-center font-bold text-x">
-      {wordleCell.char}
+    <div className={`select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl ${extraClassName[wordleCell.status]}`}>
+      {isEmpty ? ' ' : wordleCell.char}
     </div>
   )
-
 }
 
 export default Cell;
