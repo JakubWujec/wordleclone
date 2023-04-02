5ba973ea323fd16258c8563e707111158e121946
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-04-02 17:20:24 +0200

    cell component shortened

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { WordleCell, WORDLE_EMPTY_CHAR } from "../utils/wordleReducer"
 
 
@@ -15,18 +16,12 @@ let extraClassName = {
 
 const Cell = ({ wordleCell }: CellProps) => {
   const isEmpty = wordleCell.char === WORDLE_EMPTY_CHAR
+  let classNameExtra = isEmpty ? '' : extraClassName[wordleCell.status]
+  let text = isEmpty ? '' : wordleCell.char
 
-  if (isEmpty) {
-    return <div className="select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl">
-      { }
-    </div>
-  }
-
-  return (
-    <div className={`select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl ${extraClassName[wordleCell.status]}`}>
-      {isEmpty ? ' ' : wordleCell.char}
-    </div>
-  )
+  return <div className={`select-none w-12 h-12 border-2 flex justify-center items-center font-bold text-xl ${classNameExtra}`}>
+    {text}
+  </div>
 }
 
 export default Cell;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,15 @@ module.exports = {
         'wordle-unchecked-color': '#ffffff',
         'wordle-unused-color': '#D6D3D1',
       },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
