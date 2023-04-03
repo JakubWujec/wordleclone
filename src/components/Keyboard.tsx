42d26e69a82ffa3220a9e8bb222f04d12e770b89
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2023-04-03 21:34:08 +0200

    backspace replaced with icon

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -18,7 +18,7 @@ const Keyboard = (props: KeyboardProps) => {
   const keyboardRows = [
     ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'].map(l => l.toUpperCase()),
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'].map(l => l.toUpperCase()),
-    ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'BACKSPACE'].map(l => l.toUpperCase()),
+    ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'backspace'].map(l => l.toUpperCase()),
   ]
 
 
@@ -42,15 +42,23 @@ const Keyboard = (props: KeyboardProps) => {
       {keyboardRows.map((keyboardRow, index) => {
         return <KeyboardRow key={index}>
           {keyboardRow.map(char => {
+
             const keyColor = getKeyColor(char)
-            return <button key={char} className={`flex-[1_1_0%]  m-1 font-bold ${keyColor}`} onClick={props.onClick} data-value={char}>{char}
+            return <button key={char} className={`flex-[1_1_0%]  m-1 font-bold place-content-center ${keyColor}`} onClick={props.onClick} data-value={char}>
+              {char === 'BACKSPACE' ? <BackspaceIcon></BackspaceIcon> : char}
             </button>
           })}
         </KeyboardRow>
       })}
     </div>
   )
+}
 
+const BackspaceIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mx-auto pointer-events-none">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
+    </svg>)
 }
 
 export default Keyboard
